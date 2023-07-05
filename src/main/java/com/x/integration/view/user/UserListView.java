da27ecb0554137417933b067ec291c281aebdfe6
80000061 <PanLei@aacoptics.com>
2023-07-05 12:43:12 +0800

    first commit

---
+++ src/main/java/com/x/integration/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.x.integration.view.user;
+
+import com.x.integration.entity.User;
+import com.x.integration.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("INTEG_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
